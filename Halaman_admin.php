735fac9ce05e338f7a4111743241e0bc575a49a0
Tim3PPLg4 <144771313+Tim3PPLg4@users.noreply.github.com>
2023-09-18 17:17:11 +0700

    Update Halaman_admin.php

--- Halaman_admin.php
+++ Halaman_admin.php
@@ -1,1 +1,27 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Halaman Admin - www.malasngoding.com</title>
+</head>
+<body>
+    <?php 
+    session_start();
+
+    // cek apakah yang mengakses halaman ini sudah login
+    if($session['level']==""){
+    header("location:index.php?pesan=gagal");
+}
+ ?>
+ <h1>Halaman Admin</h1>
+ 
+ <p>halo <b><?php echo $_session['username'];?></b> Anda Telah login sebagai <b><?php echo $_session['level'];?></b>.</p>
+ <a href="logout.php">LOGOUT</a>
+
+
+<br/>
+<br/>
+
+  <a>Membuat login Multi Level Dengan PHP</a>
+</body>
+</html>
