8786ed202b5a71cb2efc7ca02a1e62b85a2210a6
Tim3PPLg4 <144771313+Tim3PPLg4@users.noreply.github.com>
2023-09-21 10:32:29 +0700

    Koneksi.php

--- Koneksi.php
+++ Koneksi.php
@@ -2,8 +2,8 @@
 $koneksi = mysqli_connect("localhost","root","","multi_user");
 
 // check connection
-if (mysqli_connect_errno())}
-    echo "koneksi database gagal : " . mysqli_connect_erorr();
+if (mysqli_connect_errno()){
+    echo "koneksi database gagal : " . mysqli_connect_error();
 }
 
 ?>
