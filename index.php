73e7f19b9c0df461286c46d0787cf012e2aa65ff
Tim3PPLg4 <144771313+Tim3PPLg4@users.noreply.github.com>
2023-09-26 14:31:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
        <h1>membuat login multi user level dengan php dan MySQLi <br/> www.malasngoding.com</h1>
 
 <?php
-if(isset($_GET['pesan'])) {
+if(isset($_GET['pesan'])){
     if($_GET['pesan']=="gagal"){
       echo "<div class='alert'>Username dan Password tidak sesuai !<\div>";
   }
@@ -21,7 +21,7 @@ if(isset($_GET['pesan'])) {
     <p class="tulisan_login">Login kang bener!!</p>
 
      <from action="cek_login.php" method="post">
-         <label >Username</label>
+         <label>Username</label>
           <input type="text" name="username" class="from_login" placeholder="Username .." required="required">
              
             <label>Password</label>
